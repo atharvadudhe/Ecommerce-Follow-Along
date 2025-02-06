eacc8cf272918dd14408539883acaaf426de8e6e
atharv1205 <atharvadudhe125@gmail.com>
2025-02-06 15:04:36 +0530

    completed milestone13

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage/>}/>
         <Route path='/create-product' element={<CreateProduct/>}/>
+        <Route path='/create-product/:id' element={<CreateProduct/>}/>
         <Route path='/my-products' element={<MyProducts/>} />
       </Routes>
     </BrowserRouter> 
